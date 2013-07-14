86e66bda91b4e69ed9089d94adb8925fa8549815
volnoboy <volnoboy@gmail.com>
2013-07-14 21:47:40 +0300

    GitTest

---
+++ ComVoln/src/test/java/com/volnoboy/GitTest.java
@@ -0,0 +1,11 @@
+package com.volnoboy;
+
+public class GitTest {
+	public static void main(){
+		System.out.print("Please");
+		
+	}
+public void fuckOff(){
+	System.out.println("Fuck Off!!!");
+}
+}
